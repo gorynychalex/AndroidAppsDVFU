c295f59f3857f3e0d792c095d16598d5423abf72
gorynychalex <gorynychalex@gmail.com>
2018-12-08 05:01:10 +1000

    kjfl

--- app01helloworld/src/main/java/com/example/app01helloworld/MainActivity.java
+++ app01helloworld/src/main/java/com/example/app01helloworld/MainActivity.java
@@ -7,11 +7,12 @@ package com.example.app01helloworld;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Объявление переменных для элементов активности
     TextView textView;
@@ -29,9 +30,28 @@ public class MainActivity extends AppCompatActivity {
         button.setBackgroundColor(Color.LTGRAY);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                //Нажатие на кнопку меняет из первоначального текста в измененный
+                if(p = !p) {
+                    textView.setText("Привет мир!");
+                    button.setText("Кнопка нажата");
+                    button.setBackgroundColor(Color.CYAN);
+                } else {
+                    textView.setText("Hello World!");
+                    button.setText("Кнопка");
+                    button.setBackgroundColor(Color.LTGRAY);
+                }
+                break;
+        }
+    }
+
+
+
     public void onClickButton(View view) {
 
-        //Нажатие на кнопку меняет из первоначального текста в измененный
         if(p = !p) {
             textView.setText("Привет мир!");
             button.setText("Кнопка нажата");
